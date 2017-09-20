d76c8b5b345739073306394d21b3ac3738e6a61a
David Palmquist <dpalmquist84@gmail.com>
2017-09-20 16:43:22 -0400

    initial files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+  });
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
