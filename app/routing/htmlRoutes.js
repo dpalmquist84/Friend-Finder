916347c2cc98551b16f29348d261ac69232a874b
David Palmquist <dpalmquist84@gmail.com>
2017-09-25 16:44:17 -0400

    adfas

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,13 +2,7 @@
 const path = require("path");
 
 
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
 
